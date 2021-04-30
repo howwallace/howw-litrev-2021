48e4759b5d1771987b319fc4662c444e474d60d8
howwallace <59967257+howwallace@users.noreply.github.com>
2021-04-30 23:53:58 +0200

    Update script.py

--- script.py
+++ script.py
@@ -43,25 +43,24 @@ df['se_adj'] =   df.apply(lambda row: (np.log(row['ci95_upper_adj'] -   np.log(r
 
 # separate hallucination type (CH as subtype of AH)
 df_ah = df
-df_ch = df[df['HE_short'] == 'CH']
+df_ch = df[df.HE_short == 'CH']
 
 # separate harm types (suicide as subtype of to_self)
-to_others_ah = df_ah[df_ah['harm_short'] == 'to_others']
-to_self_ah   = df_ah[(df_ah['harm_short'] == 'to_self') | (df_ah['harm_short'] == 'suicide')]
-suicide_ah   = df_ah[df_ah['harm_short'] == 'suicide']
+to_others_ah = df_ah[df_ah.harm_short == 'to_others']
+to_self_ah   = df_ah[(df_ah.harm_short == 'to_self') | (df_ah.harm_short == 'suicide')]
+# suicide_ah   = df_ah[df_ah.harm_short == 'suicide']
 
-to_others_ch = df_ch[df_ch['harm_short'] == 'to_others']
-to_self_ch   = df_ch[(df_ch['harm_short'] == 'to_self') | (df_ch['harm_short'] == 'suicide')]
-suicide_ch   = df_ch[df_ch['harm_short'] == 'suicide']
-
-
-
-to_plot = to_self_ah
-# to_plot = to_others_ah
-# to_plot = to_self_ah
+# to_others_ch = df_ch[df_ch.harm_short == 'to_others']
+to_self_ch   = df_ch[(df_ch.harm_short == 'to_self') | (df_ch.harm_short == 'suicide')]
+# suicide_ch   = df_ch[df_ch.harm_short == 'suicide']
 
 
-n_studies = len(to_plot)
+###
+# to self (AH)
+to_plot     = to_self_ah
+to_plot_ch  = to_plot[to_plot.HE_short == 'CH']
+
+n_studies   = len(to_plot)
 
 
 ### forest plot
@@ -100,7 +99,6 @@ plt.close(fig)
 ### funnel plot
 complete = to_plot[(~np.isnan(to_plot.effect_unadj)) & (~np.isnan(to_plot.se_unadj))]
 to_plot = complete
-print(to_plot)
 
 X = to_plot.effect_unadj.values
 Y = 1 / to_plot.se_unadj.values
@@ -110,21 +108,42 @@ fig, ax = plt.subplots()
 marker_scale = 1 / 8
 
 mean_odds_ratio = np.mean(X)
-ax.scatter(X, Y, s=to_plot.n_total*marker_scale, c='k', zorder=10)
+fc = ['none' if harm == 'to_self' else 'k' for harm in to_plot.harm_short.values]
+ax.scatter(X, Y, facecolors=fc, edgecolors='k', s=to_plot.n_total*marker_scale, zorder=15)
+
+X_ch = to_plot_ch.effect_unadj.values
+Y_ch = 1 / to_plot_ch.se_unadj.values
+ax.scatter(X_ch, Y_ch, c='r', s=1, zorder=20)
 
 
 # regression
 func = lambda x, m, b : m * x + b
 
-fit_params, cov = curve_fit(func, np.log(X), Y, sigma=weights)
-x_pred_lims = (-0.3, 2.8)
-log_x_pred = np.linspace(*x_pred_lims)
-y_pred = func(log_x_pred, *fit_params)
 
-ax.plot(np.exp(log_x_pred), y_pred, zorder=5, c='k')
+# def func_2(X, m_eff, m_adult, b):
+#     x_eff, x_adult = X
+#     return b + m_eff * x_eff + m_adult * x_adult
+
+X_eff   = np.log(to_plot.effect_unadj.values)
+# X_adult = to_plot.age_group.values == 'adult'
+
+X = X_eff # (X_eff, X_adult)
+Y = 1 / to_plot.se_unadj.values
+
+n = len(X) # [0])
+n_p = 2 # len(X) + 1
+
+fit_params, cov = curve_fit(func, X, Y, sigma=weights)
+print(fit_params)
+
+log_x_pred_lims = (-0.3, 2.8)
+# for age_group in range(1):
+log_x_pred = np.linspace(*log_x_pred_lims)
+y_pred = func(log_x_pred, *fit_params) # func((log_x_pred, age_group), *fit_params)
+ax.plot(np.exp(log_x_pred), y_pred, c='k', linewidth=1, zorder=5)
 
 # unweighted r**2
-residuals = Y - func(np.log(X), *fit_params)
+residuals = Y - func(X, *fit_params)
 ss_res = np.sum(residuals**2)
 ss_tot = np.sum((Y - np.mean(Y))**2)
 r_squared = 1 - (ss_res / ss_tot)
@@ -134,38 +153,37 @@ custom_lines = [ Line2D([0], [0], color='lightgray', linestyle='--'),
                  Line2D([0], [0], color='red')]
 ax.legend(custom_lines, [ 'mean (OR$=%.2f$)' % mean_odds_ratio,
                           'linear fit ($r^2=%.2f$)' % r_squared,
-                          'fitted intercept (95% C.I.)'])
+                          'fitted intercept (CI$_{95}$)'])
 
 # unweighted standard error in intercept
-n = len(X)
-n_p = 2
 if n > n_p:
-    # x_squares = np.sum(X**2) / np.sum((X - np.mean(X))**2)
-    # se_alpha = (ss_res * x_squares / (n * (n - 2)))**0.5
-    # t_val = lambda df, conf : t.ppf((1 + conf) / 2., df)
-    # ci_error_alpha = se_alpha * t_val(n - 2, 0.95)
-    # ci = (fit_params[1] - ci_error_alpha, fit_params[1] + ci_error_alpha)
-    # ax.errorbar(1, fit_params[1], ci_error_alpha, c='red', capsize=3)
+    x_squares = np.sum(X**2) / np.sum((X - np.mean(X))**2)
+    se_alpha = (ss_res * x_squares / (n * (n - 2)))**0.5
+    t_val = lambda df, conf : t.ppf((1 + conf) / 2., df)
+    ci_error_alpha = se_alpha * t_val(n - 2, 0.95)
+    ci = (fit_params[1] - ci_error_alpha, fit_params[1] + ci_error_alpha)
+    ax.errorbar(1, fit_params[1], ci_error_alpha, c='red', capsize=3, zorder=10)
 
-    X_with_intercept = np.empty(shape=(n, n_p), dtype=np.float)
-    X_with_intercept[:, 0] = 1
-    X_with_intercept[:, 1:n_p] = X
+    # X_with_intercept = np.empty(shape=(n, n_p), dtype=np.float)
+    # X_with_intercept[:, 0] = 1
+    # X_with_intercept[:, 1] = np.array(X).T
+    # # X_with_intercept[:, 1:n_p] = np.array(X).T
     
-    y_hat = func(np.log(X), *fit_params)
-    residuals = Y - y_hat
-    residual_sum_of_squares = residuals.T @ residuals
-    sigma_squared_hat = residual_sum_of_squares / (n - n_p)
-    var_beta_hat = np.linalg.inv(X_with_intercept.T @ X_with_intercept) * sigma_squared_hat
-    for p_ in range(n_p):
-        standard_error = var_beta_hat[p_, p_] ** 0.5
-        print(f"SE(beta_hat[{p_}]): {standard_error}")
+    # y_hat = func(X, *fit_params)
+    # residuals = Y - y_hat
+    # residual_sum_of_squares = residuals.T @ residuals
+    # sigma_squared_hat = residual_sum_of_squares / (n - n_p)
+    # var_beta_hat = np.linalg.inv(X_with_intercept.T @ X_with_intercept) * sigma_squared_hat
+    # for p_ in range(n_p):
+    #     standard_error = var_beta_hat[p_, p_] ** 0.5
+    #     print(f"SE(beta_hat[{p_}]): {standard_error}")
 else:
     print('n too small for se_alpha')
     
 
 
-ax.axvline(1, color='k', linestyle=':', zorder=0)
-ax.axvline(mean_odds_ratio, c='lightgray', linestyle='--', zorder=0)
+ax.axvline(1, color='k', linestyle=':', linewidth=1, zorder=0)
+ax.axvline(mean_odds_ratio, c='lightgray', linestyle='--', linewidth=1, zorder=0)
 
 ax.set_xlabel('Odds ratio (log scale)')
 ax.set_ylabel('Inverse standard error')
@@ -174,7 +192,7 @@ ax.set_ylabel('Inverse standard error')
 ax.set_xscale('log')
 ax.xaxis.set_major_formatter(mpl.ticker.ScalarFormatter())
 
-ax.set_xlim(xmin=np.exp(x_pred_lims[0]), xmax=np.exp(x_pred_lims[1]))
+ax.set_xlim(xmin=np.exp(log_x_pred_lims[0]), xmax=np.exp(log_x_pred_lims[1]))
 # ax.set_ylim(ymin=0, ymax=4.3)
 
 fig.tight_layout()
